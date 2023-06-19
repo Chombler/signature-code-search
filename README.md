31705fd9c749f00f445680a8f5a4e4e12554ba41
Chombler <64819823+Chombler@users.noreply.github.com>
2023-06-19 12:16:46 -0400

    Greater documentation

--- README.md
+++ README.md
@@ -1,10 +1,27 @@
 # signature code search
- Demo of github code search for model hub signatures
+This is a demo of [Github Code Search](https://github.com/search) using [Anaconda](https://www.anaconda.com/download) as a virtual environment
+and [PyGithub](https://github.com/PyGithub/PyGithub) to abstract the API calls
 
-Activate environment using:
+Activate the Anaconda environment using:
 
 ``` conda activate ./env```
 
-Run query example once the environment is activated with:
+Run [demo](query.py) once the environment is activated with:
 
 ```python3 search.py```
+
+The [demo](query.py) performs 2 queries that look for HuggingFace signatures using the GitHub code search API.
+The **MODULE_SEARCH_QUERY** searches for the huggingface_hub api being included as a module within any projects,
+while the **API_SEARCH_QUERY** looks for any calls to the huggingface website.
+These queries were constructed using the help of the [REST API search endpoint](https://docs.github.com/en/rest/search?apiVersion=2022-11-28)
+and [GitHub Code Search Syntax](https://docs.github.com/en/search-github/github-code-search/understanding-github-code-search-syntax)
+
+# IMPORTANT
+It is worth noting that there is a discrepancy in results returned by each query.
+Each query when performed through the github website returns 50k+ files, but only seems to return around 800 when running the [demo](query.py).
+We are unsure why, but a good place to start might be in what is actually indicated by ```results.totalCount```.
+It may that this returns the number of pages, and that the actually number of files is consistent regardles of how the query is made.
+
+Here is the [module query](https://github.com/search?type=code&auto_enroll=true&q=import+huggingface_hub+language%3Apython+) when made through the website
+
+Here is the [api_query](https://github.com/search?type=code&auto_enroll=true&q=huggingface.co+language%3Apython) when made through the website
--- search.py
+++ search.py
@@ -8,7 +8,8 @@ GITHUB_TOKEN = "YOUR_TOKEN_HERE"
 
 # Create a search query to find all Python repos that use HuggingFace
 # https://docs.github.com/en/github/searching-for-information-on-github/searching-on-github/searching-code
-SEARCH_QUERY = "import huggingface_hub language:python"
+MODULE_SEARCH_QUERY = "import huggingface_hub language:python"
+API_SEARCH_QUERY = "huggingface.co language:python"
 
 # Create a Github instance using an access token
 gh = Github(GITHUB_TOKEN)
@@ -16,18 +17,26 @@ gh = Github(GITHUB_TOKEN)
 # Search code, returns a paginated list of github.ContentFile.ContentFile objects
 # https://pygithub.readthedocs.io/en/latest/github.html#github.Github.search_code
 # https://pygithub.readthedocs.io/en/latest/github_objects/ContentFile.html
-results = gh.search_code(query=SEARCH_QUERY)
+def signature_search(gh, query, output_file="results.txt"):
 
-print(results.totalCount)
+    results = gh.search_code(query=query)
 
-# Print the first 100 results using pagination, write to a file
-# https://pygithub.readthedocs.io/en/latest/utilities.html#github.PaginatedList.PaginatedList
-# https://pygithub.readthedocs.io/en/latest/github_objects/Repository.html#github.Repository.Repository
-i = 0
-with open("results.txt", "w") as output_file:
-    output_file.write(f"Total results: {results.totalCount}\n")
-    for file in results:
-        if i > 100:
-            break
-        i += 1
-        output_file.write(f"{file.repository.git_url}: {file.path}\n")
+    # Print the total number of results
+    # !!! IMPORTANT !!! Unsure what this is actually counting
+    print(f"Total results: {results.totalCount}")
+
+    # Get the first 100 results using pagination, write to a file
+    # https://pygithub.readthedocs.io/en/latest/utilities.html#github.PaginatedList.PaginatedList
+    # https://pygithub.readthedocs.io/en/latest/github_objects/Repository.html#github.Repository.Repository
+    i = 0
+    with open(output_file, "w") as OUTPUT_FILE:
+        OUTPUT_FILE.write(f"Total results: {results.totalCount}\n")
+        for file in results:
+            if i > 100:
+                break
+            i += 1
+            OUTPUT_FILE.write(f"{file.repository.git_url}: {file.path}\n")
+
+if __name__ == "__main__":
+    signature_search(gh, MODULE_SEARCH_QUERY, "module_results.txt")
+    signature_search(gh, API_SEARCH_QUERY, "api_results.txt")
